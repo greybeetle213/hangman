3c39354330d918251e4b2ee746e5da891c68002f
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2020-08-20 14:04:29 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function main(event){
-    var inputedLetter = document.getElementById("input").value.toLowerCase()
+    var inputedLetter = document.getElementById("input").value
     if (event.key == "Enter" && inputedLetter != "" && gameOver == false){
         if (wordToGuess.includes(inputedLetter)){
                 var indexesToChange = []
@@ -15,6 +15,7 @@ function main(event){
             document.getElementById("word").innerHTML = oldletters.join("")
             if (oldletters.includes("_") == false){
                 gameOver = true
+                document.getElementById("word").style.color = "green"
                 document.getElementById("reload").style.display = "unset"
             }
         } else {
@@ -28,8 +29,10 @@ function main(event){
                 ctx.arc(hangmanDrawing[wrongGuesses][1]*canvasScale,hangmanDrawing[wrongGuesses][2]*canvasScale,hangmanDrawing[wrongGuesses][3]*canvasScale,0,2*Math.PI)
             }
             ctx.stroke()
-            if(wrongGuesses == 9){
+            if(wrongGuesses == 10){
                 gameOver = true
+                document.getElementById("word").innerHTML = wordToGuess.join(" ")
+                document.getElementById("word").style.color = "red"
                 document.getElementById("reload").style.display = "unset"
             }
         }
@@ -43,26 +46,25 @@ function init(){
     document.getElementById("input").addEventListener("keydown", function(event){main(event)})
     ctx = document.getElementById("hangmanCanvas").getContext("2d")
     canvasScale = document.getElementById("hangmanCanvas").width/100
-    hangmanDrawing = [[5,95,30,95],[17,95,17,20],[17,20,83,20],[83,20,83,30],['circle',83,40,10], [83,50,83,80], [83,50,73,60], [83,50,93,60], [83,80,73,95], [83,80,93,95]]
-    var xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-        wordToGuess = xhttp.responseText
-        wordToGuess = wordToGuess.split("\n")
-        wordToGuess = wordToGuess[Math.floor(Math.random()*wordToGuess.length)]
-        wordToGuess = wordToGuess.toLowerCase()
-        wordToGuess = wordToGuess.split("")
-        wordToGuess.pop()
+    hangmanDrawing = [[5,95,30,95],[17,95,17,20],[17,20,83,20],[17,30,37,20],[83,20,83,30],['circle',83,40,10], [83,50,83,80], [83,50,73,60], [83,50,93,60], [83,80,73,95], [83,80,93,95]]
+    // var xhttp = new XMLHttpRequest()
+    // xhttp.onreadystatechange = function() {
+    // if (this.readyState == 4 && this.status == 200) {
+    //     // Typical action to be performed when the document is ready:
+    //     wordToGuess = xhttp.responseText
+    //     wordToGuess = wordToGuess.split("\n")
+    //     wordToGuess = wordToGuess[Math.floor(Math.random()*wordToGuess.length)]
+    //     wordToGuess = wordToGuess.split("")
+        wordToGuess = ["r","e","e","e","e","e","e"]
         wrongGuesses = -1
         gameOver = false
         for (counter = wordToGuess.length; counter > 0; counter -= 1){
             document.getElementById("word").innerHTML += "_ "
         }
-        }
-    }
-    xhttp.open("GET", "nouns.csv", true);
-    xhttp.send();
+    //     }
+    // }
+    // xhttp.open("GET", "nouns.csv", true);
+    // xhttp.send();
 
 
 }
